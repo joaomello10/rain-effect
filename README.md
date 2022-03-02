aaa7b833158a55608e4b07359f8b82bf6c0905e2
João <mellofontesjp16@gmail.com>
2022-03-02 16:37:30 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,32 @@
+<h1 align='center'>Rain Effect</h1>
+
+<h2 align='center'>About</h2>
+
+<h3 align='center'>Rain effect using the html canvas</h3>
+
+<img align='center' src='img/rain-effect.gif'>
+
+<br></br>
+
+<h2 align='start'>You can change the rain values</h2>
+
+<code>lengthOfRain = 100;</code>
+<img align='center' src='img/rain-effect-100.gif'>
+<br></br>
+<code>lengthOfRain = 300;</code>
+<img align='center' src='img/rain-effect-300.gif'>
+
+## 🚀 Technologiess
+
+<ul>
+    <li>Javascript</li>
+    <li>html</li>
+    <li>css</li>
+</ul>
+
+<div align='center'>
+  <h3>My linkedin</h3>
+  <a href="https://www.linkedin.com/in/joao-pedro-mello/" target='_blank'><img src='https://img.shields.io/badge/LinkedIn-0077B5?style=for-the-badge&logo=linkedin&logoColor=white'/></a>
+</div>
+
+Made by João Pedro
---
+++ app.js
@@ -0,0 +1,63 @@
+const canvas = document.querySelector("canvas");
+const context = canvas.getContext("2d");
+let lengthOfRain = 300;
+
+function randomNumber(b, a) {
+  return Math.floor(Math.random() * (b - a + 1)) + a;
+}
+
+function createObject(x, y, color, width, height) {
+  context.fillStyle = color;
+  context.fillRect(x, y, width, height);
+}
+
+class Rain {
+  constructor() {
+    this.x = randomNumber(canvas.width, 1);
+    this.y = randomNumber(1000, 1) * -1;
+    this.color = this.x % 2 == 0 ? "white" : "#999";
+    this.width = randomNumber(8, 5);
+    this.height = this.width * 6;
+    this.rotate = randomNumber(3, 1);
+  }
+  draw() {
+    createObject(this.x, this.y, this.color, this.width, this.height);
+  }
+}
+
+function fall() {
+  const rain = new Rain();
+  const speed = randomNumber(30, 20);
+  function loop() {
+    rain.y += speed;
+    if (rain.rotate % 2 == 0) {
+      rain.x += 1;
+    } else {
+      rain.x -= 1;
+    }
+    if (rain.y > canvas.height) {
+      rain.x = randomNumber(canvas.width, 1);
+      rain.y = randomNumber(1000, 1) * -1;
+      rain.rotate = randomNumber(3, 1);
+    }
+    rain.draw();
+    window.requestAnimationFrame(loop);
+  }
+
+  loop();
+}
+
+function run() {
+  createObject(0, 0, "#333", canvas.width, canvas.height);
+  window.requestAnimationFrame(run);
+}
+
+window.addEventListener("load", () => {
+  for (let i = 0; i < lengthOfRain; i++) {
+    setTimeout(() => {
+      fall();
+    }, 1000);
+  }
+});
+
+run();
